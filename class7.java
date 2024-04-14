e7fd3803e499045a267b8c6ee1ad8d4aca2fcece
Ganesh Dutt <gdutt2304@gmail.com>
2024-04-14 14:09:20 +0530

    Add another class

--- class7.java
+++ class7.java
@@ -5,15 +5,29 @@ import java.util.Scanner;
 public class class7 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter team1 marks..");
-        int team1 = sc.nextInt();
+//        System.out.println("Enter team1 marks..");
+//        int team1 = sc.nextInt();
+//
+//        System.out.println("Enter team2 marks..");
+//        int team2 = sc.nextInt();
+//      int result  = printD(team1,team2);
+//        System.out.println(result);
 
-        System.out.println("Enter team2 marks..");
-        int team2 = sc.nextInt();
-      int result  = printD(team1,team2);
-        System.out.println(result);
+
+//        method2
+
+//        int add = sum(14,65);
+//        System.out.println("Function add.. "+ add);
+//
+//        System.out.println(" again call methods.."+sum(76,98));
+
+//        method 3 (Another class)
+
+
+        class8.AnotherClass();
+
     }
-
+//first method
     public static int printD(int team1, int team2) {
 
         if (team1 >= team2) {
@@ -23,5 +37,13 @@ public class class7 {
             System.out.println("Team 2 win ...." + team2);
             return (team2);
         }
+
+
+    }
+
+
+//    method2
+    public  static int sum(int a , int b){
+        return (a + b);
     }
 }
---
+++ class8.java
@@ -0,0 +1,7 @@
+package intro;
+
+public class class8 {
+    public static void AnotherClass() {
+        System.out.println("Another Aman");
+    }
+}
