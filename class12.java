c9e5a45bbe8357cfdc8af5bc35bc30a130a72089
Ganesh Dutt <gdutt2304@gmail.com>
2024-05-04 14:30:27 +0530

    Array

---
+++ class12.java
@@ -0,0 +1,25 @@
+package intro;
+
+public class class12 {
+    public static void main(String[] args) {
+//        String str = "yash";
+//        String str1 = "shivang";
+//        String str2 = "aman";
+//        String  str3 = "Yash, Aman , shivang ";
+
+
+        int [] arr = {12,23,54,343,322,434,76,98};
+//        System.out.println(arr[1] + "   "+ arr[0] +" "+ arr[3]);
+
+//        System.out.println( "23"+ 12 + 22);
+
+//        for(int x : arr){
+//            System.out.print(x + " ");
+//        }
+
+        for(int i = 0; i < arr.length; i++){
+            System.out.print(arr[i]+ " ");
+        }
+
+    }
+}
