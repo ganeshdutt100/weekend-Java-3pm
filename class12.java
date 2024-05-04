c9908bb067f0fc03f7ef34cba49f3462c312d462
Ganesh Dutt <gdutt2304@gmail.com>
2024-05-04 15:03:36 +0530

    Update Array

--- class12.java
+++ class12.java
@@ -8,7 +8,7 @@ public class class12 {
 //        String  str3 = "Yash, Aman , shivang ";
 
 
-        int [] arr = {12,23,54,343,322,434,76,98};
+        int[] arr = {12, 23, 54, 343, 322, 434, 76, 98};
 //        System.out.println(arr[1] + "   "+ arr[0] +" "+ arr[3]);
 
 //        System.out.println( "23"+ 12 + 22);
@@ -17,9 +17,27 @@ public class class12 {
 //            System.out.print(x + " ");
 //        }
 
-        for(int i = 0; i < arr.length; i++){
-            System.out.print(arr[i]+ " ");
+//        for(int i = 0; i < arr.length; i++){
+//            System.out.print(arr[i]+ " ");
+//        }
+
+        String str[] = {"Yash", "Shivang", " Aman", "Nikhil"};
+        for (String a : str) {
+            System.out.println(a);
+        }
+
+        int num [] = new int[9];
+        num[1] = 12;
+        num[0]= 11;
+        num[2]= 13;
+        num[3]= 14;
+        num[4]= 15;
+//        num[5]= 16;
+
+        for(int x : num) {
+            System.out.print(x + " ");
         }
 
     }
+
 }
