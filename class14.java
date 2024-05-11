4aa8f4bff398085252d32dffedf5c52090eca3f3
Ganesh Dutt <gdutt2304@gmail.com>
2024-05-11 15:07:34 +0530

    2d array

--- class14.java
+++ class14.java
@@ -11,12 +11,42 @@ public class class14 {
 //        2 3 4 5
 //        3 5 2 3
 
-     int a[][]= {
-             {1,2,3},{2,5,3},{5,7,8}
+     int []a[]= {
+             {1,2,3},
+             {2,5,3},
+             {5,7,8}
      };
        for( int i=0; i<a.length; i++){
            for(int j = 0; j<a[i].length; j++){
                System.out.print(a[i][j]+ " ");
+           }
+           System.out.println();
+       }
+
+
+       int arr1[][]= {
+               {1,2,3,5,6,3,32,2},
+               {2,5,3},
+               {5,7,8,4,566,3,2}
+     };
+       for( int i=0; i<arr1.length; i++){
+           for(int j = 0; j<arr1[i].length; j++){
+               System.out.print(arr1[i][j]+ " ");
+           }
+           System.out.println();
+       }
+
+
+
+       int arr3[][] = new int[3][];
+
+       arr3[0] = new int[]{2,5,6};
+       arr3[1] = new int[]{2,5,6,6,4,3};
+       arr3[2] = new int[]{2,5,6};
+
+       for(int i=0; i<arr3.length; i++){
+           for(int j = 0; j<arr3[i].length; j++){
+               System.out.print(arr3[i][j] + " ");
            }
            System.out.println();
        }
