193856d795ac60e39ccfe653d86c5aab427bcbfc
Ganesh Dutt <gdutt2304@gmail.com>
2024-05-11 15:52:26 +0530

    Jugged array

--- class14.java
+++ class14.java
@@ -37,22 +37,54 @@ public class class14 {
        }
 
 
+//
+//       int arr3[][] = new int[3][];
+//
+//       arr3[0] = new int[]{2,5,6};
+//       arr3[1] = new int[]{2,5,6,6,4,3};
+//       arr3[2] = new int[]{2,5,6};
+//
+//       for(int i=0; i<arr3.length; i++){
+//           for(int j = 0; j<arr3[i].length; j++){
+//               System.out.print(arr3[i][j] + " ");
+//           }
+//           System.out.println();
+//       }
 
-       int arr3[][] = new int[3][];
-
-       arr3[0] = new int[]{2,5,6};
-       arr3[1] = new int[]{2,5,6,6,4,3};
-       arr3[2] = new int[]{2,5,6};
-
-       for(int i=0; i<arr3.length; i++){
-           for(int j = 0; j<arr3[i].length; j++){
-               System.out.print(arr3[i][j] + " ");
-           }
-           System.out.println();
-       }
 
 
-    }
+                Scanner scanner = new Scanner(System.in);
+                int arr3[][] = new int[3][];
 
-    }
+
+                System.out.println("Enter your first 3 value ");
+                arr3[0] = new int[3];
+                for (int j = 0; j < 3; j++) {
+                    arr3[0][j] = scanner.nextInt();
+                }
+
+                System.out.println("Enter second 6 value :");
+                arr3[1] = new int[6];
+                for (int j = 0; j < 6; j++) {
+                    arr3[1][j] = scanner.nextInt();
+                }
+
+                System.out.println("Enter your third 3 value  : ");
+                arr3[2] = new int[3];
+                for (int j = 0; j < 3; j++) {
+                    arr3[2][j] = scanner.nextInt();
+                }
+
+                for (int i = 0; i < arr3.length; i++) {
+                    for (int j = 0; j < arr3[i].length; j++) {
+                        System.out.print(arr3[i][j] + " ");
+                    }
+                    System.out.println();
+                }
+
+
+            }
+        }
+
+
 
