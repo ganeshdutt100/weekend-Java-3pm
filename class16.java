0e37a10795697729cbee43b1c8ddc034c1f34b05
Ganesh Dutt <gdutt2304@gmail.com>
2024-05-11 16:02:48 +0530

    2d Array

---
+++ class16.java
@@ -0,0 +1,10 @@
+package intro;
+
+import java.util.Arrays;
+
+public class class16 {
+    public static void main(String[] args) {
+        int a[] = {1,3,5,6,6};
+        System.out.println(Arrays.toString(a));
+    }
+}
