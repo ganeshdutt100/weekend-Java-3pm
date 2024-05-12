e0fa1e54f6f2ce7b25d2709e72e64d00a585af52
Ganesh Dutt <gdutt2304@gmail.com>
2024-05-12 14:23:08 +0530

    Array method

---
+++ class17.java
@@ -0,0 +1,41 @@
+package intro;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class class17 {
+    public static void main(String[] args) {
+        int a[] = {1, 2, 3, 44, 0};
+//        int  []d = Arrays.copyOf(a,8);
+        int  []d = Arrays.copyOfRange(a,2,5);
+        System.out.println(Arrays.toString(d));
+//        int c[] = {1, 2, 3, 4,0};
+  int b [] = new int[7];
+  b[0] = 1;
+  b[1] = 28;
+  b[2] = 39;
+  b[3] = 4;
+  for(int i = 0; i < b.length; i++){
+      System.out.print(b[i]+ " ");
+  }
+   for(int x : b){
+       System.out.print(x);
+   }
+//        System.out.println(Arrays.toString(b));
+//  b[4] = 02;
+        Arrays.fill(b,78 );
+        System.out.println(Arrays.equals(a,b));
+        System.out.println(Arrays.toString(b));
+//        System.out.println(Arrays.fill(45));
+
+        System.out.println(Arrays.toString(a));
+
+
+        //        Arrays.sort(a);
+//        System.out.println(Arrays.toString(a));
+//        System.out.println(Arrays.binarySearch(a,75));
+//        for(int x :  a){
+//            System.out.print(x);
+//        }
+    }
+}
