ed8e73c583c66a0869f1383d787a6c841d566485
Ganesh Dutt <gdutt2304@gmail.com>
2024-05-12 15:18:27 +0530

    Update questions

--- class17.java
+++ class17.java
@@ -37,5 +37,17 @@ public class class17 {
 //        for(int x :  a){
 //            System.out.print(x);
 //        }
+
+        System.out.println(fun(a,54));
+    }
+
+    public static boolean fun(int arr[]  , int value){
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] == value){
+                return true;
+            }
+
+        }
+        return false;
     }
 }
