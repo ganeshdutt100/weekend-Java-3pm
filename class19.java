54585356613742351aea8f4999d8250ee3e15eff
Ganesh Dutt <gdutt2304@gmail.com>
2024-05-31 12:12:25 +0530

    update arrays clone

--- class19.java
+++ class19.java
@@ -10,9 +10,18 @@ public class class19 {
 //        System.arraycopy(a,0,b, 2,2 );
 //        System.out.println(Arrays.toString(b));
 
-        int[] a = {1, 2, 3, 54, 25,16, 7};
-        int[] b =  Arrays.copyOf(a, a.length);
-        System.out.println(Arrays.toString(b));
+
+//        copyOf
+//        int[] a = {1, 2, 3, 54, 25,16, 7};
+//        int[] b =  Arrays.copyOf(a, a.length);
+//        System.out.println(Arrays.toString(b));
+
+//        clone()
+        int[] a = {1, 2, 3, 54, 25,16, 7,8};
+        int[] b =  a.clone();
+        a[4]= 34;
+        System.out.println ("Original Array : "+ Arrays.toString(a));
+        System.out.println( "shallowCopyArray :"+ Arrays.toString(b));
 
     }
 }
